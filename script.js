5e8580339d4266b188e45f2d2f397bdcbbabb0cd
“Anahi <parrilla2552@gmail.com>
2022-09-26 10:59:03 -0500

    chore: add script

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko Papi Saoko");
+document.getElementById("generation").style.color = "grey";
+function colorTitulo(){
+  console.log("Pulso el botón de generation");
+  document.getElementById("generation").style.color = "blue";
+}
